4f5c4b0bfb7611ece35da3a5853e92917e884c57
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-01-09 17:36:41 +0900

    Write setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name="ipynb_open",
+    version="0.1.0",
+    description="Open ipynb file on an existing server",
+    author="Toshiki Teramura",
+    author_email="toshiki.teramura+ipynb_open@gmail.com",
+    license="MIT",
+    keywords="utility",
+    url="http://github.com/termoshtt/ipynb_open",
+    packages=["ipynb_open"],
+    scripts=["ipynb-open"],
+)
